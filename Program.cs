94d163f1569a4e4b91ba2cc46af1fb81c75ee348
vlnn <anohin.va@gmail.com>
2019-03-15 11:40:59 +0200

    Store output color and reset console before exit

--- Program.cs
+++ Program.cs
@@ -3,15 +3,44 @@ using System.IO;
 
 namespace ttt {
     class Program {
+        static string CurrentDirectory = Directory.GetCurrentDirectory();
+        static ConsoleColor CurrentForeground;
+        static ConsoleColor CurrentBackground;
+
         static void Main(string[] args) {
-            var currentDirectory = Directory.GetCurrentDirectory();
-            Console.WriteLine("ttt starts in: " + currentDirectory);
-            Console.WriteLine();
-            string[] filePaths = Directory.GetFiles(currentDirectory);
+            InitConsole();
+            ListCurrentDir();
+            ReleaseConsole();
+        }
+
+        private static void ReleaseConsole() {
+            Console.ResetColor();
+        }
+
+        private static void ListCurrentDir() {
+            string[] filePaths = Directory.GetFiles(CurrentDirectory);
             for (int i = 0; i < filePaths.Length; ++i) {
                 string path = filePaths[i];
                 Console.WriteLine(System.IO.Path.GetFileName(path));
             }
+        }
+
+        private static void InitConsole() {
+            CurrentForeground = Console.ForegroundColor; // init when needed
+            CurrentBackground = Console.BackgroundColor;
+            SwitchToTechText();
+            Console.WriteLine("ttt starts in: " + CurrentDirectory);
+            Console.WriteLine();
+            SwitchToFileText();
+        }
+
+        private static void SwitchToFileText() {
+            Console.ForegroundColor = CurrentForeground;
+            Console.BackgroundColor = CurrentBackground;
+        }
+
+        private static void SwitchToTechText() {
+            Console.ForegroundColor = ConsoleColor.Green;
         }
     }
 }
