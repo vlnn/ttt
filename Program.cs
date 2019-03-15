08aaa3290c921d1c09fc88f4c5d11c5838fea992
vlnn <anohin.va@gmail.com>
2019-03-15 10:33:08 +0200

    Output header and file list

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ttt {
+    class Program {
+        static void Main(string[] args) {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            Console.WriteLine("ttt starts in: " + currentDirectory);
+            Console.WriteLine();
+            string[] filePaths = Directory.GetFiles(currentDirectory);
+            for (int i = 0; i < filePaths.Length; ++i) {
+                string path = filePaths[i];
+                Console.WriteLine(System.IO.Path.GetFileName(path));
+            }
+        }
+    }
+}
